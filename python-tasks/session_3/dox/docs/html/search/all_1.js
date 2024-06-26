69447a39ddb434ff97a49a84f1a5af628bd2705e
hady Mohamed <57275439+hady-potter@users.noreply.github.com>
2024-06-27 02:34:16 +0300

    Delete python-tasks/session_3/dox directory

--- python-tasks/session_3/dox/docs/html/search/all_1.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['sub_2',['sub',['../test_8h.html#aafe27965474c250fa0d582779f130d57',1,'test.c']]],
-  ['sum_3',['sum',['../test_8h.html#a5ceb2172cbc407c45e7e9f2d22a2b5c1',1,'test.c']]]
-];
